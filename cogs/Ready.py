444bf209f29f87d9878dbf9090f4f834521e9670
Hassan Shirazi <shiraziahassan@gmail.com>
2020-10-24 22:07:25 -0400

    Add load_extension for cogs

---
+++ cogs/Ready.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext.commands import Cog
+
+
+class Ready(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @Cog.listener()
+    async def on_ready(self):
+        print("Hashashin is online.")
+
+
+def setup(bot):
+    bot.add_cog(Ready(bot))
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from discord.ext.commands import Bot
 from dotenv import load_dotenv
 from discord.utils import get
 from discord.errors import ClientException
+from discord.ext.commands import Cog
+
 
 load_dotenv()
 
@@ -22,9 +24,9 @@ bot = commands.Bot(command_prefix="$")
 client = discord.Client()
 
 
-@bot.event
-async def on_ready():
-    print("Hashashin is online.")
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f"cogs.{extension}")
 
 
 @bot.command(pass_context=True, aliases=["join"])
@@ -67,5 +69,9 @@ async def print(ctx, *args):
 )
 async def ping(ctx):
     await ctx.send("pong")
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith('.py'):
+        bot.load_extension(f"cogs.{filename[:-3]}")
 
 bot.run(Discord_token)
