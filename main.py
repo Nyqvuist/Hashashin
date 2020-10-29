201c44d45221ea879c06772c75abc890c840014a
Ahmad Raza <razatastic@gmail.com>
2020-10-28 21:31:15 -0400

    Remove unused imports, lint

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 import discord
 import logging
-import typing
 import os
-import youtube_dl
-import asyncio
 
-from discord.ext import commands
-from discord.ext.commands import Bot
 from dotenv import load_dotenv
-from discord.utils import get
-from discord.errors import ClientException
-from discord.ext.commands import Cog
+from discord.ext.commands import Bot
 
-
-load_dotenv()
-
-Discord_token = os.getenv("DISCORD_TOKEN")
+load_dotenv()  # Load environment variables
 
 logging.basicConfig(level=logging.INFO)
 
-bot = commands.Bot(command_prefix="$")
+bot = Bot(command_prefix="$")
 
 client = discord.Client()
 
@@ -28,16 +18,9 @@ client = discord.Client()
 async def load(ctx, extension):
     bot.load_extension(f"cogs.{extension}")
 
-
-@bot.command(
-    help="The Yin to my Yang"
-)
-async def ping(ctx):
-    await ctx.send("pong")
-
-
+# Load all cogs in the cogs directory
 for filename in os.listdir("./cogs"):
     if filename.endswith('.py'):
         bot.load_extension(f"cogs.{filename[:-3]}")
 
-bot.run(Discord_token)
+bot.run(os.getenv("DISCORD_TOKEN"))
