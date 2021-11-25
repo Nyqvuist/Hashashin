192e8813d4d5fa9df07ad8a34a4585721b4e65ed
Hassan Shirazi <shiraziahassan@gmail.com>
2021-11-25 18:25:39 -0500

    .

--- bot.py
+++ bot.py
@@ -9,9 +9,9 @@ load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-LAVALINK_HOST = os.getenv("LAVALINK_HOST")
-LAVALINK_PASSWORD = os.getenv("LAVALINK_PASSWORD")
-LAVALINK_PORT = os.getenv("LAVALINK_PORT")
+LAVALINK_HOST = os.environ("LAVALINK_HOST")
+LAVALINK_PASSWORD = os.environ("LAVALINK_PASSWORD")
+LAVALINK_PORT = os.environ("LAVALINK_PORT")
 
 logging.basicConfig(level=logging.INFO)
 
@@ -52,9 +52,9 @@ async def on_shard_ready(
     """Event that triggers when the hikari gateway is ready."""
     builder = (
         lavasnek_rs.LavalinkBuilder(event.my_user.id, DISCORD_TOKEN)
-        .set_host(os.environ["LAVALINK_HOST"])
-        .set_password(os.environ["LAVALINK_PASSWORD"])
-        .set_port(int(os.environ["LAVALINK_PORT"]))
+        .set_host(LAVALINK_HOST)
+        .set_password(LAVALINK_PASSWORD)
+        .set_port(int(LAVALINK_PORT))
         .set_start_gateway(False)
         # We set start gateway False because hikari can handle
         # voice events for us.
