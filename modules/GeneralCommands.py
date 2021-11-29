8181d4726342ca0131bce8247ffe9cafc08af759
Hassan Shirazi <shiraziahassan@gmail.com>
2021-11-29 18:30:51 -0500

    .

--- modules/GeneralCommands.py
+++ modules/GeneralCommands.py
@@ -1,17 +1,46 @@
 import tanjun
 import hikari
 import random
+import typing
+
+from tanjun.parsing import Argument
 
 component = tanjun.Component()
 
 
 @component.with_slash_command
-@tanjun.as_slash_command("d20", "Roll a d20 dice.")
-async def roll_d20(ctx: tanjun.abc.SlashContext) -> None:
 
-    roll = random.randint(1, 20)
+@tanjun.with_int_slash_option("coin", "Flip a coin", default=None)
+@tanjun.with_int_slash_option("d6", "Roll a d6 dice.", default=None)
+@tanjun.with_int_slash_option("d20", "Roll a d20 dice.", default=None)
+@tanjun.as_slash_command("roll", "Roll a dice")
+async def roll_d20(ctx: tanjun.abc.SlashContext, d20:typing.Optional[int], d6:typing.Optional[int], coin:typing.Optional[int]) -> None:
 
-    await ctx.respond(ctx.author + " rolled a " + str(roll) + "!", delete_after=60)
+    if d20:
+
+        roll = random.randint(1, 20)
+
+        await ctx.respond("**" + ctx.member.display_name + "**" + " rolled a " + str(roll) + "!", delete_after=60)
+
+    elif d6:
+
+        roll = random.randint(1, 6)
+
+        await ctx.respond("**" + ctx.member.display_name + "**" + " rolled a " + str(roll) + "!", delete_after=60)
+    
+    elif coin:
+
+        coinflip = ['Heads','Tails']
+
+        roll = random.choice(coinflip)
+
+        await ctx.respond("**" + ctx.member.display_name + "**" + " flipped " + str(roll) + "!", delete_after=60)
+    
+    else:
+
+        await ctx.respond("Please choose a dice or coin!", delete_after=30)
+
+
     
 
 @tanjun.as_loader
