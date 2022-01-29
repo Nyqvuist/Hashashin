7f5b4b6390129ead344a9ab5f9b402b63ac5e26a
Hassan Shirazi <shiraziahassan@gmail.com>
2022-01-29 18:37:04 -0500

    added scheduler

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import os
 import logging
 from dotenv import load_dotenv
 import lavasnek_rs
+from datetime import datetime
+import asyncio
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 load_dotenv()
 
@@ -104,5 +107,27 @@ for filename in os.listdir("./modules"):
         client.load_modules(f"modules.{filename[:-3]}")
 
 
+async def tick():
+    await client.rest.create_message(
+        channel=767527790802632714,
+        content="test message to message @The Three Time",
+        role_mentions=True
+    )
+
+
+async def sched_start():
+    loop = asyncio.get_running_loop()
+
+    results = await loop.run_in_executor(None, scheduler.start)
+
+    return results
+
+
+if __name__ == '__main__':
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(tick, 'cron', day_of_week='6',hour='12', minute='30')
+    scheduler.start()
+
 
 bot.run()
+
