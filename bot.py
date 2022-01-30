fe1a914578289debde69fa71caeb92e6f8daacbb
Hassan Shirazi <shiraziahassan@gmail.com>
2022-01-29 19:12:36 -0500

    test

--- bot.py
+++ bot.py
@@ -29,25 +29,25 @@ async def on_started(event: hikari.StartedEvent) -> None:
 
 client.load_modules(*Path("./modules").glob("*.py"))
 
-async def tick():
-    await client.rest.create_message(
-        channel=767527790802632714,
-        content="test message to message @The Three Time",
-        role_mentions=True
-    )
+# async def tick():
+#     await client.rest.create_message(
+#         channel=767527790802632714,
+#         content="test message to message @The Three Time",
+#         role_mentions=True
+#     )
 
-scheduler = AsyncIOScheduler()
-scheduler.add_job(tick, 'cron', day_of_week='6',hour='12', minute='30')
+# scheduler = AsyncIOScheduler()
+# scheduler.add_job(tick, 'cron', day_of_week='6',hour='12', minute='30')
 
-async def sched_start():
-    loop = asyncio.get_running_loop()
+# async def sched_start():
+#     loop = asyncio.get_running_loop()
 
-    results = await loop.run_in_executor(None, scheduler.start)
+#     results = await loop.run_in_executor(None, scheduler.start)
 
-    return results
+#     return results
 
 
-scheduler.start()
+# scheduler.start()
 
 
 bot.run()
